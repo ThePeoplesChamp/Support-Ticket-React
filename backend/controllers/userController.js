eda5415032874835718160f26d49b77810e868f0
Peoples Champion <95397105+ThePeoplesChamp@users.noreply.github.com>
2022-07-09 10:45:50 +0930

    More final touches

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,29 +4,35 @@ const jwt = require('jsonwebtoken')
 
 const User = require('../models/userModel')
 
+// @desc    Register a new user
+// @route   /api/users
+// @access  Public
 const registerUser = asyncHandler(async (req, res) => {
     const { name, email, password } = req.body
 
+    // Validation
     if (!name || !email || !password) {
         res.status(400)
         throw new Error('Please include all fields')
     }
 
-
+    // Find if user already exists
     const userExists = await User.findOne({ email })
 
     if (userExists) {
         res.status(400)
-        throw new Error('user already exists')
+        throw new Error('User already exists')
     }
 
+    // Hash password
     const salt = await bcrypt.genSalt(10)
     const hashedPassword = await bcrypt.hash(password, salt)
 
+    // Create user
     const user = await User.create({
         name,
         email,
-        password: hashedPassword
+        password: hashedPassword,
     })
 
     if (user) {
@@ -34,7 +40,7 @@ const registerUser = asyncHandler(async (req, res) => {
             _id: user._id,
             name: user.name,
             email: user.email,
-            token: generatedToken(user._id),
+            token: generateToken(user._id),
         })
     } else {
         res.status(400)
@@ -42,17 +48,21 @@ const registerUser = asyncHandler(async (req, res) => {
     }
 })
 
+// @desc    Login a user
+// @route   /api/users/login
+// @access  Public
 const loginUser = asyncHandler(async (req, res) => {
     const { email, password } = req.body
 
     const user = await User.findOne({ email })
 
+    // Check user and passwords match
     if (user && (await bcrypt.compare(password, user.password))) {
-        res.status(201).json({
+        res.status(200).json({
             _id: user._id,
             name: user.name,
             email: user.email,
-            token: generatedToken(user._id)
+            token: generateToken(user._id),
         })
     } else {
         res.status(401)
@@ -60,8 +70,9 @@ const loginUser = asyncHandler(async (req, res) => {
     }
 })
 
-
-
+// @desc    Get current user
+// @route   /api/users/me
+// @access  Private
 const getMe = asyncHandler(async (req, res) => {
     const user = {
         id: req.user._id,
@@ -71,12 +82,15 @@ const getMe = asyncHandler(async (req, res) => {
     res.status(200).json(user)
 })
 
-// Generate token 
+// Generate token
 const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {
-        expiresIn: '30d'
+        expiresIn: '30d',
     })
 }
 
-
-module.exports = { getMe, loginUser, registerUser }
+module.exports = {
+    registerUser,
+    loginUser,
+    getMe,
+}
